8f8ec318e055109b8475541054a08a5a57eb64c6
Stefan Isaksson <stefan.foo.isaksson@gmail.com>
2019-01-11 15:17:32 +0100

    Added a POST service to create new pokemons

--- pokemon/README.md
+++ pokemon/README.md
@@ -4,9 +4,10 @@
 
 |Service                          |Type  |Description                                             |
 |---------------------------------|------|--------------------------------------------------------
-|/generations                     |GET   |Lists pokemon generations                               |
+|/generations/                    |GET   |Lists pokemon generations                               |
 |/generations/<generation_number> |GET   |List of pokemons for given generation                   |
 |/pokemons/<pokedex_number>       |GET   |Pokemon information, e.g. attack and defense            |
+|/pokemons/                       |POST  |Adds a pokemon                                          |
 
 ## Requires Flask and Pandas
 `pip install Flask`
--- pokemon/app.py
+++ pokemon/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request, Response
 import sqlite3
 import pandas as pd
+import json
 
 app = Flask(__name__)
 
@@ -9,31 +10,68 @@ def get_db_connection():
     return sqlite3.connect("pokemons.db")
 
 
-@app.route('/generations')
+@app.route('/generations/')
 def get_generations():
     df = pd.read_sql_query("select generation, count(name) number_of_pokemons from pokemons group by generation;",
-                           index_col='generation',
                            con=get_db_connection())
-    return df.to_json(orient='table')
+    return df.to_json(orient='records')
 
 
 @app.route('/generations/<int:generation>')
 def get_pokemons_by_generation(generation):
     df = pd.read_sql_query("select pokedex_number, name from pokemons where generation=?;",
                            params=[(generation)],
-                           index_col='pokedex_number',
                            con=get_db_connection())
-    return df.to_json(orient='table')
+    return df.to_json(orient='records')
 
 
-@app.route('/pokemons/<int:pokedex_number>')
+@app.route('/pokemons/<int:pokedex_number>', methods=['GET'])
 def get_pokemon_by_pokedex_number(pokedex_number):
     print(pokedex_number)
-    df = pd.read_sql_query("select pokedex_number, name, type1, type2 from pokemons where pokedex_number = ?;",
+    df = pd.read_sql_query("select pokedex_number, name, generation, type1, type2, is_legendary, "
+                           "hp, attack, defense, sp_attack, sp_defense, speed from pokemons where pokedex_number = ?;",
                            params={pokedex_number},
                            index_col='pokedex_number',
                            con=get_db_connection())
-    return df.to_json(orient='table')
+    return df.to_json(orient='records')
+
+
+def valid_pokemon(pokemon):
+    if "pokedex_number" in pokemon and "name" in pokemon \
+            and "generation" in pokemon and "type1" in pokemon\
+            and "type2" in pokemon and "is_legendary" in pokemon:
+        return True
+    else:
+        return False
+
+
+@app.route('/pokemons/', methods=['POST'])
+def add_pokemon():
+    request_data = request.get_json()
+    if valid_pokemon(request_data):
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute('''INSERT INTO pokemons(name, pokedex_number, generation, type1, type2, is_legendary)
+                          VALUES(?,?,?,?,?,?)''',
+                       (request_data['name'],
+                        request_data['pokedex_number'],
+                        request_data['generation'],
+                        request_data['type1'],
+                        request_data['type2'],
+                        request_data['is_legendary']))
+        db.commit()
+        response = Response("",status=201, mimetype='application/json')
+        return response
+    else:
+        error_msg = {
+            "error" : "Invalid pokemon passed in request",
+            "helpString" : "Data passed should look similar to this: "
+                           "{'name':'Bulbasaur','generation':1,'type1':'grass','type2':'poison',"
+                           "'is_legendary':0,'hp':45,'attack':49,'defense':49,'sp_attack':65,"
+                           "'sp_defense':65,'speed':45}"
+        }
+        response = Response(json.dumps(error_msg), status=400, mimetype='application/json')
+        return response
 
 
 if __name__ == "__main__":
